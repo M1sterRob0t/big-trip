8a7cc0fa84739aeac4a9e8f8a4fb4208bd273f5b
Alexander Lusto <you@example.com>
2023-01-18 11:48:23 +0300

    Вынес логику по созданию и отрисовке точек маршрута в отдельные функции, реализовал замену точки маршрута на карточку редоктирования

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ import {render, RenderPosition} from "./utils";
 
 const EVENTS_NUMBER = 15;
 const points = generatePoints(EVENTS_NUMBER);
-console.log(points);
-const renderDays = (events) => {
+
+const renderDays = (container, events) => {
   if (events.length === 0) {
     return;
   }
@@ -23,34 +23,62 @@ const renderDays = (events) => {
 
   for (let i = 1; i <= days; i++) {
     const dayComponent = new Day(date, i);
-    render(tripDays, dayComponent.getElement());
+    render(container, dayComponent.getElement());
     date = new Date(+date + day);
   }
 };
 
-const renderPointsByDay = (events) => {
-  const tripEventsLists = tripDays.querySelectorAll(`.trip-events__list`);
+const renderPointsByDay = (container, events) => {
+  const tripEventsLists = container.querySelectorAll(`.trip-events__list`);
   const day = 1000 * 60 * 60 * 24;
   let date = events.at(0).dateFrom;
 
   for (let i = 0; i < tripEventsLists.length; i++) { // проходимся по всем дням
     for (let j = 0; j < events.length; j++) { // проходимся по всем событиям
       if (date.getDate() === events[j].dateFrom.getDate() && date.getMonth() === events[j].dateFrom.getMonth()) { // если дата одинаковая
-        const eventComponent = new Event(events[j]);
-        render(tripEventsLists[i], eventComponent.getElement()); // рендер в этот день
+        renderEvent(tripEventsLists[i], events[j]); // рендер в этот день
       }
     }
     date = new Date(+date + day);
   }
 };
 
+const renderEvent = (eventsList, event) => {
+  const eventComponent = new Event(event);
+  const eventEditComponent = new EventEdit(event);
+
+  const eventRollupButtonClickHandler = () => {
+    eventsList.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+  };
+
+  const eventEditRollupButtonClickHandler = () => {
+    eventsList.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+  };
+
+  const eventRollupButton = eventComponent.getElement().querySelector(`.event__rollup-btn`);
+  const eventEditForm = eventEditComponent.getElement();
+
+  eventRollupButton.addEventListener(`click`, eventRollupButtonClickHandler);
+  eventEditForm.addEventListener(`submit`, eventEditRollupButtonClickHandler);
+
+  render(eventsList, eventComponent.getElement()); // рендер в этот день
+};
+
+const renderTrip = (container, events) => {
+  const sortComponent = new Sort();
+  const daysComponent = new Days(points);
+
+  render(container, sortComponent.getElement(), RenderPosition.BEFOREEND);
+  render(container, daysComponent.getElement(), RenderPosition.BEFOREEND);
+
+  const tripDays = tripEvents.querySelector(`.trip-days`);
+  renderDays(tripDays, events);
+  renderPointsByDay(tripDays, events);
+};
+
 const tripInfoComponent = new TripInfo(points);
 const tabsComponent = new Tabs();
 const filtersComponent = new Filters();
-const sortComponent = new Sort();
-const daysComponent = new Days(points);
-const eventEditComponent = new EventEdit(points[0]);
-
 
 const tripMain = document.querySelector(`.trip-main`);
 const tripControlsHeaders = tripMain.querySelectorAll(`.trip-main__trip-controls h2`);
@@ -59,10 +87,5 @@ const tripEvents = document.querySelector(`.trip-events`);
 render(tripMain, tripInfoComponent.getElement(points), RenderPosition.AFTERBEGIN);
 render(tripControlsHeaders[0], tabsComponent.getElement(), RenderPosition.AFTEREND);
 render(tripControlsHeaders[1], filtersComponent.getElement(), RenderPosition.AFTEREND);
-render(tripEvents, sortComponent.getElement(), RenderPosition.BEFOREEND);
-render(tripEvents, eventEditComponent.getElement(), RenderPosition.BEFOREEND);
-render(tripEvents, daysComponent.getElement(), RenderPosition.BEFOREEND);
 
-const tripDays = tripEvents.querySelector(`.trip-days`);
-renderDays(points);
-renderPointsByDay(points);
+renderTrip(tripEvents, points);
